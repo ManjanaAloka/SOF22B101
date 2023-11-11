6e4fae00d59bdbf0aa03584254a2a204a8fc7200
Manjana <manjanaaloka12345@gmail.,com>
2023-11-12 02:42:13 +0530

    final

--- function/function.cpp
+++ function/function.cpp
@@ -9,13 +9,28 @@ int main()
     return 0;
 }
 
-void myFunc(){
-    string myArray[] = {"ab","cd","asd"};
-    int arraySize = sizeof(myArray);
-    std::cout << arraySize/32;
-    // for (size_t i = 0; i < count; i++)
-    // {
-    //     /* code */
-    // }
-    
+void myFunc()
+{
+
+    std::cout << "Enter Your Array Lenth : ";
+    int len;
+    string bd[len] = {"2002/07/05","2002/09/06"};
+    cin >> len;
+    string myArray[len] = {};
+    int i = 1;
+    string val;
+    while (i <= len)
+    {
+        string val;
+        std::cout << "Value " << i << " : ";
+        cin >> val;
+        myArray[i] = {val};
+        i++;
+    }
+
+    std::cout <<"Array size is :"<< len << "\n";
+    for (int i = 0; i < len; i++)
+    {
+        std::cout << myArray[i] <<" Your Birth Day is - "<<bd[i]<< "\n";
+    }
 }
