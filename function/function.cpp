76a0bf1caa6ed17896aa078fd852e11d1a7b92a8
Manjana <manjanaaloka12345@gmail.,com>
2023-11-11 19:48:34 +0530

    function

---
+++ function/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void myFunc();
+
+int main()
+{
+    myFunc();
+    return 0;
+}
+
+void myFunc(){
+    string myArray[] = {"manja","nimneth","isuru"};
+}
